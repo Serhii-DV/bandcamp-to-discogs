5da5d1e9846b2c6f90a3bfe9b7eecfeb7d36ae97
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-06-14 14:59:27 +0200

    Convert to manifest version 3

--- background.js
+++ background.js
@@ -1,23 +1,23 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log("The color is green.");
-	});
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
 
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-		  conditions: [
-			new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {
-					hostContains: 'bandcamp.com'
-				},
-			}),
-			new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {
-					hostContains: 'discogs.com'
-				},
-			})
-		  ],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {
+          hostContains: 'bandcamp.com'
+        },
+      }),
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {
+          hostContains: 'discogs.com'
+        },
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
 });

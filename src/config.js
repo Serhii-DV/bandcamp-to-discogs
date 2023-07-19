32dba6fd78f8175f7f6d49e5a924742ac7b4d7fe
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-07-19 11:19:43 +0200

    Always use local about.html file

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ export default {
   genres_url: '../data/discogs_genres.json',
   // keyword_mapping_url: '../data/keyword_mapping.json',
   keyword_mapping_url: 'https://gist.githubusercontent.com/Serhii-DV/44181f307548aabac2703147d3c730ba/raw/mapping.json',
-  // about_url: '../data/about.html',
-  about_url: 'https://gist.githubusercontent.com/Serhii-DV/44181f307548aabac2703147d3c730ba/raw/about.html',
+  about_url: '../data/about.html'
 };

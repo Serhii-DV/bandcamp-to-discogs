69d1eca7b1a6c88fbdf8c90ba642bab7e28e750f
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-07-19 11:20:49 +0200

    Always use the latest gist version

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // genres_url: 'https://gist.githubusercontent.com/Serhii-DV/14d7ec13fd15e30db1a2a8dff047abbf/raw/02fe41ab5ea493202bb423fef3640d5d8394af63/discogs_genres_and_styles.json',
+  // genres_url: 'https://gist.githubusercontent.com/Serhii-DV/14d7ec13fd15e30db1a2a8dff047abbf/raw/discogs_genres_and_styles.json',
   genres_url: '../data/discogs_genres.json',
   // keyword_mapping_url: '../data/keyword_mapping.json',
   keyword_mapping_url: 'https://gist.githubusercontent.com/Serhii-DV/44181f307548aabac2703147d3c730ba/raw/mapping.json',

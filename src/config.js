557351f81137aeaecd43d755e5f8812f5803f789
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-07-19 10:34:17 +0200

    Fixed github permanent link

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   // genres_url: 'https://gist.githubusercontent.com/Serhii-DV/14d7ec13fd15e30db1a2a8dff047abbf/raw/02fe41ab5ea493202bb423fef3640d5d8394af63/discogs_genres_and_styles.json',
   genres_url: '../data/discogs_genres.json',
   // keyword_mapping_url: '../data/keyword_mapping.json',
-  keyword_mapping_url: 'https://gist.githubusercontent.com/Serhii-DV/44181f307548aabac2703147d3c730ba/raw/b40b0eab42aa16f9ad4ea13570421a5e7945c117/mapping.json',
+  keyword_mapping_url: 'https://gist.githubusercontent.com/Serhii-DV/44181f307548aabac2703147d3c730ba/raw/mapping.json',
   // about_url: '../data/about.html',
-  about_url: 'https://gist.githubusercontent.com/Serhii-DV/44181f307548aabac2703147d3c730ba/raw/49f79caa854825dd09af30a346f0e6069734056d/about.html',
+  about_url: 'https://gist.githubusercontent.com/Serhii-DV/44181f307548aabac2703147d3c730ba/raw/about.html',
 };

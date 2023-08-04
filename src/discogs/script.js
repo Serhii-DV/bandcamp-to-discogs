b8f39da07af2f9a0c991d1b58bf7aef7801bab97
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-08-04 15:17:03 +0200

    Added logic that should automatically fill some fields in the draft

--- src/discogs/script.js
+++ src/discogs/script.js
@@ -11,6 +11,7 @@
     updateQuantity(trackNumInputs.length);
     selectFormatFileType('FLAC');
     selectFormatDescription('Album');
+    fillDurations();
   }, 2000);
 
   function detectElements() {
@@ -24,9 +25,7 @@
    * @param {Number} qty
    */
   function updateQuantity(qty) {
-    const oldQty = qtyInput.value;
-    qtyInput.value = qty;
-    console.log('B2D: Quantity has been updated from ' + oldQty + ' to ' + qty);
+    setInputValue(qtyInput, qty);
   }
 
   /**
@@ -36,7 +35,7 @@
     const fileTypeInput = document.querySelector('input[value="' + fileType + '"]');
 
     if (fileTypeInput) {
-      fileTypeInput.checked = true;
+      checkInput(fileTypeInput);
       console.log(`B2D: Format file type ${fileType} was checked`);
     } else {
       console.log(`B2D: Format file type ${fileType} not found`);
@@ -50,10 +49,67 @@
     const descriptionInput = document.querySelector('.format_descriptions_type input[value="' + formatDescription + '"]');
 
     if (descriptionInput) {
-      descriptionInput.checked = true;
+      checkInput(descriptionInput);
       console.log(`B2D: Format description ${formatDescription} was checked`);
     } else {
       console.log(`B2D: Format description ${formatDescription} not found`);
     }
   }
+
+  function fillDurations() {
+    trackDurationInputs.forEach((durationInput, index) => {
+      if (durationInput.value !== '') {
+        return;
+      }
+
+      const trackTitleInput = trackTitleInputs[index];
+      const [title, duration] = extractTitleAndTime(trackTitleInput.value);
+
+      setInputValue(trackTitleInput, title);
+      setInputValue(durationInput, duration);
+    });
+  }
 })(document);
+
+/**
+ * @param {String} str
+ * @returns {Array<String, String>}
+ */
+function extractTitleAndTime(str) {
+  const parts = str.split(" ");
+
+  // Check if the last part matches the time format (minutes:seconds)
+  const timeFormatRegex = /^\d+:\d+$/; // Example: 7:38
+  const lastPart = parts[parts.length - 1];
+
+  if (!timeFormatRegex.test(lastPart)) {
+    return [str, ''];
+  }
+
+  const timeValue = parts.pop();
+  const modifiedString = parts.join(" ");
+
+  return [modifiedString, timeValue];
+}
+
+function setInputValue(inputElement, value) {
+  const oldValue = inputElement.value;
+  inputElement.focus();
+  inputElement.value = value;
+  triggerInputEvent(inputElement);
+  inputElement.blur();
+
+  console.log(`B2D: Value changed from "${oldValue}" to "${value}"`);
+}
+
+function checkInput(inputElement) {
+  inputElement.focus();
+  inputElement.click();
+  triggerInputEvent(inputElement);
+  inputElement.blur();
+}
+
+function triggerInputEvent(element) {
+  const inputEvent = new Event("input", { bubbles: true, cancelable: true });
+  element.dispatchEvent(inputEvent);
+}

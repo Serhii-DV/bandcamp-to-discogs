c9587aa2c1d74aa324605185cee65e9f3f19cc0c
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2023-10-24 13:10:08 +0200

    Don't ignore dist folder

---
+++ src/popup/vendors.js
@@ -0,0 +1,5 @@
+// Custom components
+import { B2DIconComponent } from './components/icon.js';
+import { ConsoleCommand } from './components/console-command.js';
+import { ExternalContent } from './components/external-content.js';
+import { ReleasesList } from './components/releases-list.js';
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: {
+    vendors: './src/popup/vendors.js',
     popup: './src/popup/popup.js',
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'source-map',
 };

5693107d5fe555c468b6dd2aa54522b530e013b8
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-07-19 00:02:42 +0200

    Fix getting element by id

--- src/discogs/script.js
+++ src/discogs/script.js
@@ -37,7 +37,7 @@
   }
 
   function detectElements() {
-    artistNameInput = document.getElementById('#artist-name-input');
+    artistNameInput = document.getElementById('artist-name-input');
     qtyInput = document.querySelector('input[aria-label="Quantity of format"]');
     trackTitleInputs = document.querySelectorAll('.track_input');
     trackDurationInputs = document.querySelectorAll('input[aria-label="Track duration"]');

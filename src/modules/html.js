bc0a0747a22cafd121bd8f7ecc4f9fc9a25da865
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-07-24 21:47:06 +0200

    Log injected css file

--- src/modules/html.js
+++ src/modules/html.js
@@ -195,6 +195,9 @@ export function injectCSSFile(cssUrl) {
   const linkElement = document.createElement('link');
   linkElement.rel = 'stylesheet';
   linkElement.href = cssUrl;
+  linkElement.onload = () => {
+    console.log('[B2D] Injected css file:', cssUrl);
+  };
 
   document.head.appendChild(linkElement);
 }

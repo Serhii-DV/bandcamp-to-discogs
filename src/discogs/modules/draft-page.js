a997e19c8f7d75574aaa58d31ac45f734a7b049c
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-07-24 15:14:29 +0200

    Moved some methods to the draft page module

--- src/discogs/modules/draft-page.js
+++ src/discogs/modules/draft-page.js
@@ -9,3 +9,90 @@ export function getReleaseTitleInput() {
 export function getSubmissionFormSectionNotes() {
   return document.querySelector('#subform .notes');
 }
+
+/**
+ * @param {String} fileType
+ */
+export function selectFormatFileType(fileType) {
+  const fileTypeInput = document.querySelector('input[value="' + fileType + '"]');
+
+  if (fileTypeInput) {
+    checkInput(fileTypeInput);
+    console.log(`B2D: Format file type ${fileType} was checked`);
+  } else {
+    console.log(`B2D: Format file type ${fileType} not found`);
+  }
+}
+
+/**
+ * @param {String} formatDescription
+ */
+export function selectFormatDescription(formatDescription) {
+  const descriptionInput = document.querySelector('.format_descriptions_type input[value="' + formatDescription + '"]');
+
+  if (descriptionInput) {
+    checkInput(descriptionInput);
+    console.log(`B2D: Format description ${formatDescription} was checked`);
+  } else {
+    console.log(`B2D: Format description ${formatDescription} not found`);
+  }
+}
+
+export function fillDurations() {
+  const trackDurationInputs = document.querySelectorAll('input[aria-label="Track duration"]');
+
+  trackDurationInputs.forEach((durationInput, index) => {
+    if (durationInput.value !== '') {
+      return;
+    }
+
+    const trackTitleInput = trackTitleInputs[index];
+    const [title, duration] = extractTitleAndTime(trackTitleInput.value);
+
+    setInputValue(trackTitleInput, title);
+    setInputValue(durationInput, duration);
+  });
+}
+
+/**
+ * @param {String} str
+ * @returns {Array<String, String>}
+ */
+function extractTitleAndTime(str) {
+  const parts = str.split(" ");
+
+  // Check if the last part matches the time format (minutes:seconds)
+  const timeFormatRegex = /^\d+:\d+$/; // Example: 7:38
+  const lastPart = parts[parts.length - 1];
+
+  if (!timeFormatRegex.test(lastPart)) {
+    return [str, ''];
+  }
+
+  const timeValue = parts.pop();
+  const modifiedString = parts.join(" ");
+
+  return [modifiedString, timeValue];
+}
+
+export function setInputValue(inputElement, value) {
+  const oldValue = inputElement.value;
+  inputElement.focus();
+  inputElement.value = value;
+  triggerInputEvent(inputElement);
+  inputElement.blur();
+
+  console.log(`B2D: Value changed from "${oldValue}" to "${value}"`);
+}
+
+function checkInput(inputElement) {
+  inputElement.focus();
+  inputElement.click();
+  triggerInputEvent(inputElement);
+  inputElement.blur();
+}
+
+function triggerInputEvent(element) {
+  const inputEvent = new Event("input", { bubbles: true, cancelable: true });
+  element.dispatchEvent(inputEvent);
+}
--- src/discogs/script.js
+++ src/discogs/script.js
@@ -1,13 +1,12 @@
 'use strict';
 
 import { click } from "../modules/html.js";
-import { getSubmissionFormSectionNotes } from "./modules/draft-page.js";
+import { fillDurations, getSubmissionFormSectionNotes, selectFormatDescription, selectFormatFileType, setInputValue } from "./modules/draft-page.js";
 
 export function runScript() {
   let artistNameInput;
   let qtyInput;
   let trackTitleInputs;
-  let trackDurationInputs;
   let notesTextarea;
   let submissionFormSectionNotes;
   let submissionNotesTextarea;
@@ -24,7 +23,6 @@ export function runScript() {
     artistNameInput = document.getElementById('artist-name-input');
     qtyInput = document.querySelector('input[aria-label="Quantity of format"]');
     trackTitleInputs = document.querySelectorAll('.track_input');
-    trackDurationInputs = document.querySelectorAll('input[aria-label="Track duration"]');
     notesTextarea = document.querySelector('textarea#release-notes-textarea');
     submissionFormSectionNotes = getSubmissionFormSectionNotes();
     submissionNotesTextarea = document.querySelector('textarea#release-submission-notes-textarea');
@@ -83,48 +81,6 @@ export function runScript() {
     setInputValue(qtyInput, qty);
   }
 
-  /**
-   * @param {String} fileType
-   */
-  function selectFormatFileType(fileType) {
-    const fileTypeInput = document.querySelector('input[value="' + fileType + '"]');
-
-    if (fileTypeInput) {
-      checkInput(fileTypeInput);
-      console.log(`B2D: Format file type ${fileType} was checked`);
-    } else {
-      console.log(`B2D: Format file type ${fileType} not found`);
-    }
-  }
-
-  /**
-   * @param {String} formatDescription
-   */
-  function selectFormatDescription(formatDescription) {
-    const descriptionInput = document.querySelector('.format_descriptions_type input[value="' + formatDescription + '"]');
-
-    if (descriptionInput) {
-      checkInput(descriptionInput);
-      console.log(`B2D: Format description ${formatDescription} was checked`);
-    } else {
-      console.log(`B2D: Format description ${formatDescription} not found`);
-    }
-  }
-
-  function fillDurations() {
-    trackDurationInputs.forEach((durationInput, index) => {
-      if (durationInput.value !== '') {
-        return;
-      }
-
-      const trackTitleInput = trackTitleInputs[index];
-      const [title, duration] = extractTitleAndTime(trackTitleInput.value);
-
-      setInputValue(trackTitleInput, title);
-      setInputValue(durationInput, duration);
-    });
-  }
-
   // Notifications
 
   const notificationStack = createNotificationStack();
@@ -158,49 +114,6 @@ export function runScript() {
   function showNotificationWarning(message) { showNotification(message, 'warning'); }
   function showNotificationError(message) { showNotification(message, 'error'); }
 
-}
-
-/**
- * @param {String} str
- * @returns {Array<String, String>}
- */
-function extractTitleAndTime(str) {
-  const parts = str.split(" ");
-
-  // Check if the last part matches the time format (minutes:seconds)
-  const timeFormatRegex = /^\d+:\d+$/; // Example: 7:38
-  const lastPart = parts[parts.length - 1];
-
-  if (!timeFormatRegex.test(lastPart)) {
-    return [str, ''];
-  }
-
-  const timeValue = parts.pop();
-  const modifiedString = parts.join(" ");
-
-  return [modifiedString, timeValue];
-}
-
-function setInputValue(inputElement, value) {
-  const oldValue = inputElement.value;
-  inputElement.focus();
-  inputElement.value = value;
-  triggerInputEvent(inputElement);
-  inputElement.blur();
-
-  console.log(`B2D: Value changed from "${oldValue}" to "${value}"`);
-}
-
-function checkInput(inputElement) {
-  inputElement.focus();
-  inputElement.click();
-  triggerInputEvent(inputElement);
-  inputElement.blur();
-}
-
-function triggerInputEvent(element) {
-  const inputEvent = new Event("input", { bubbles: true, cancelable: true });
-  element.dispatchEvent(inputEvent);
 }
 
 function isObject(value) {

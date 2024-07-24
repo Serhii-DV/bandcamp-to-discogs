d4b61fcf78d0cda7a4b6daced937a42f0d95a24f
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-07-24 15:06:50 +0200

    Added more data to the metadata

--- src/discogs/app/metadata.js
+++ src/discogs/app/metadata.js
@@ -1,12 +1,28 @@
 import { Release } from "../../app/release.js";
 import { getExtensionManifest } from "../../modules/chrome.js";
 import { generateSubmissionNotes } from "../modules/discogs.js";
+import { getDiscogsDateValue } from "./utils.js";
 
 export class Metadata {
   /**
    * @param {String}
    */
   version;
+
+  /**
+   * @param {String}
+   */
+  artists;
+
+  /**
+   * @param {String}
+   */
+  title;
+
+  /**
+   * @param {String}
+   */
+  label;
 
   /**
    * @param {Object}
@@ -16,23 +32,64 @@ export class Metadata {
   /**
    * @param {String}
    */
+  country;
+
+  /**
+   * @param {String}
+   */
+  released;
+
+  /**
+   * @param {String}
+   */
+  tracklist;
+
+  /**
+   * @param {String}
+   */
+  credits;
+
+  /**
+   * @param {String}
+   */
+  genres;
+
+  /**
+   * @param {String}
+   */
   submissionNotes;
 
   constructor({
+    artists,
+    title,
+    label,
     trackQty,
     formatFileType,
     formatDescription,
+    country,
+    released,
+    tracklist,
+    credits,
+    genres,
     releaseUrl
   }) {
     const self = this;
     const manifest = getExtensionManifest();
 
     self.version = manifest.version;
+    self.artists = artists;
+    self.title = title;
+    self.label = label;
     self.format = {
       qty: trackQty,
       fileType: formatFileType,
       description: formatDescription
     };
+    self.country = country;
+    self.released = released;
+    self.tracklist = tracklist;
+    self.credits = credits;
+    self.genres = genres;
     self.submissionNotes = generateSubmissionNotes(releaseUrl);
   }
 
@@ -42,10 +99,18 @@ export class Metadata {
    * @return {Metadata} - The converted Metadata instance.
    */
   static fromRelease(release) {
+    const publishedDate = getDiscogsDateValue(release.published);
+    const modifiedDate = getDiscogsDateValue(release.modified);
+
     return new Metadata({
+      artists: release.releaseItem.artist,
+      title: release.releaseItem.title,
+      label: release.label,
+      released: `Published date: ${publishedDate}. Modified date: ${modifiedDate}`,
       trackQty: release.tracksQty,
       formatFileType: 'FLAC',
       formatDescription: 'Album',
+      genres: release.keywords.join(', '),
       releaseUrl: release.url
     });
   }

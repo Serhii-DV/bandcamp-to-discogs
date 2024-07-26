1c105f6ac9269514e25515451e57c79bc583ccf2
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-07-26 11:42:56 +0200

    Wait for loading placeholder before initialization

--- src/discogs/modules/content-main.js
+++ src/discogs/modules/content-main.js
@@ -9,31 +9,30 @@ export function main () {
   injectCSSFile(chrome.runtime.getURL('src/discogs/css/b2d.css'));
   injectCSSFile(chrome.runtime.getURL('src/discogs/notification.css'));
 
-  showNotificationInfo('Initialization...');
+  showNotificationInfo('Waiting for metadata parsing...');
 
-  const html = document.querySelector('html');
+  // Check for Loading placeholder and run initialization
+  const checkInterval = 1000;
+  const intervalId = setInterval(() => {
+    const loadingElement = document.querySelector('.loading-placeholder');
+    if (loadingElement) return;
 
-  observeAttributeChange(html, 'class', (el) => {
-    if (el.classList.contains('de-enabled') && !el.classList.contains('b2d-initialized')) {
-      // Initialize the script after some period of time. We have to wait for elements initializing on the page.
-      setTimeout(() => {
-        try {
-          initialize();
-        } catch (error) {
-          console.error(error);
-          showNotInitializedMessage();
-        }
-      }, 5000);
-      el.classList.add('b2d-initialized');
+    try {
+      initialize();
+    } catch (error) {
+      console.error(error);
+      showNotInitializedMessage(error.message);
     }
-  });
+
+    clearInterval(intervalId); // Stop checking
+  }, checkInterval);
 
   setupSendMessageToPopup();
 }
 
-const showNotInitializedMessage = () => {
+const showNotInitializedMessage = (message) => {
   showNotificationError(
-    'Not initialized!<br><button class="button button-small button-blue action-restart">Restart initialization</button>',
+    message + '<br><button class="button button-small button-blue action-restart">Restart initialization</button>',
     (notification) => {
       notification
         .querySelector('.action-restart')

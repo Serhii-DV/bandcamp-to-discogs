1ecb0a36655d6455240a4f23d9e895d751f25215
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-07-30 15:43:28 +0200

    Fixed console initialization

--- src/popup/console.js
+++ src/popup/console.js
@@ -3,27 +3,44 @@ import { releaseToDiscogsCsv } from "../discogs/modules/discogs.js";
 import { logStorage } from "../modules/storage.js";
 import { logInfo } from "../utils/console.js";
 
-const consoleCommand = document.querySelector('console-command');
-
 export function setupConsole() {
-  logInfo('Setup console');
-  consoleCommand.addCommand('log.storage', () => { logStorage();});
+  initConsole((consoleCommand) => {
+    consoleCommand.addCommand('log.storage', () => { logStorage();});
+  })
 }
 
 /**
  * @param {Release} release
  */
 export function setupConsoleRelease(release, keywordsMapping) {
-  consoleCommand.addCommand('log.release', () => {
-    console.log(release);
-  });
+  initConsole((consoleCommand) => {
+    consoleCommand.addCommand('log.release', () => {
+      console.log(release);
+    });
 
-  consoleCommand.addCommand('log.keywordsMapping', () => {
-    console.log(keywordsMapping);
-  });
+    consoleCommand.addCommand('log.keywordsMapping', () => {
+      console.log(keywordsMapping);
+    });
 
-  const discogsCsv = releaseToDiscogsCsv(release);
-  consoleCommand.addCommand('log.discogsCsvNotes', () => {
-    console.log(discogsCsv.notes);
+    const discogsCsv = releaseToDiscogsCsv(release);
+    consoleCommand.addCommand('log.discogsCsvNotes', () => {
+      console.log(discogsCsv.notes);
+    });
   });
+}
+
+function initConsole(onInit) {
+  logInfo('Init console');
+
+  // TODO: find a better solution
+  // Wait for dashboard content load
+  const interval = 100;
+  const intervalId = setInterval(() => {
+    const consoleCommand = document.querySelector('console-command');
+    if (consoleCommand) {
+      onInit(consoleCommand);
+      logInfo('Console initialized');
+      clearInterval(intervalId);
+    }
+  }, interval);
 }

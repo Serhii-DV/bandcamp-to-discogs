ab52e7a1b522a987a53823158f073461c376edb3
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-08-05 15:22:35 +0200

    Convert JS to TS in `key-generator.ts`

--- src/utils/key-generator.ts
+++ src/utils/key-generator.ts
@@ -1,42 +1,31 @@
+import { ReleaseItem } from 'src/app/release';
+import { Release } from 'src/app/release';
 import { v5 as uuid5} from 'uuid';
-import { Release } from '../app/release.js';
 
-/**
- * @param {String} url
- * @returns {String}
- */
-export function generateKeyForUrl(url) {
+export function generateKeyForUrl(url: string): string {
   const uuid = uuid5(url, uuid5.URL);
   // console.log(`B2D: uuid5(${url}): `, uuid);
   return uuid;
 }
 
-/**
- * @param {ReleaseItem} releaseItem
- * @return {String}
- */
-export function generateKeyForReleaseItem(releaseItem) {
+export function generateKeyForReleaseItem(releaseItem: ReleaseItem): string {
   return generateKeyForUrl(releaseItem.url);
 }
 
-/**
- * @param {Release} release
- * @return {String}
- */
-export function generateKeyForRelease(release) {
+export function generateKeyForRelease(release: Release): string {
   return generateKeyForReleaseItem(release.releaseItem);
 }
 
-export function generateKeysFromUrls(urls) {
-  const keys = [];
-  urls.forEach(url => keys.push(generateKeyForUrl(url)));
+export function generateKeysFromUrls(urls: string[]): string[] {
+  const keys: string[] = [];
+  urls.forEach((url: string) => keys.push(generateKeyForUrl(url)));
   return keys;
 }
 
-export function generateKeyUrlMapFromUrls(urls) {
-  const keyUrlMap = {};
+export function generateKeyUrlMapFromUrls(urls: string[]): Record<string, string> {
+  const keyUrlMap: Record<string, string> = {};
 
-  urls.forEach(url => {
+  urls.forEach((url: string) => {
     const key = generateKeyForUrl(url);
     keyUrlMap[key] = url;
   });

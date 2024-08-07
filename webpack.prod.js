d6ffe0240d8aaed8af3ee1477e3093c20d947732
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-08-07 22:12:53 +0200

    Back to one config. Now get mode version from CLI

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.NODE_ENV = 'production';
-
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-  devtool: 'source-map',
-});

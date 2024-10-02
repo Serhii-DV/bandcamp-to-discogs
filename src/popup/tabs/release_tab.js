4544457b0a073ccd36e9a87ea99e26e1fe994f28
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-10-02 22:50:59 +0200

    Refactor rendering release card

--- src/popup/tabs/release_tab.js
+++ src/popup/tabs/release_tab.js
@@ -6,7 +6,6 @@ import {
 } from '../../utils/utils';
 import { setBackgroundImage } from '../helpers.js';
 import { setupBtnToDownloadReleasesAsCsv } from './download_tab.js';
-import { createElementFromHTML } from '../../utils/html';
 
 /**
  * @param {Element} tab
@@ -20,37 +19,13 @@ export function setupReleaseTab(tab, release, btnDownloadRelease) {
     hideWarning();
   }
 
-  outputRelease(tab, release);
+  renderReleaseCard(release, tab.querySelector('main'));
   setupBtnToDownloadReleasesAsCsv(btnDownloadRelease, [release]);
 }
 
 function hideWarning() {
   const warningEl = document.getElementById('b2d-warning-no-data');
   warningEl.classList.add('visually-hidden');
-}
-
-/**
- * @param {Element} tab
- * @param {Release} release
- */
-function outputRelease(tab, release) {
-  const mainElement = addReleaseCardToElement(
-    release,
-    tab.querySelector('main')
-  );
-  const releaseArtist = mainElement.querySelector('#release-artist');
-  const releaseTitle = mainElement.querySelector('#release-title');
-
-  setBackgroundImage(document.querySelector('.bg-image'), release.image);
-
-  let countArtistLines = countLinesInHtmlElement(releaseArtist);
-  let countTitleLines = countLinesInHtmlElement(releaseTitle);
-
-  releaseArtist.classList.toggle(
-    'display-6',
-    countArtistLines >= 3 && countArtistLines <= 5
-  );
-  mainElement.classList = 'lines-a' + countArtistLines + '-t' + countTitleLines;
 }
 
 function countLinesInHtmlElement(el) {
@@ -62,9 +37,10 @@ function countLinesInHtmlElement(el) {
 /**
  * @param {Release} release
  * @param {Element} element
- * @returns {Element}
  */
-function addReleaseCardToElement(release, element) {
+function renderReleaseCard(release, element) {
+  setBackgroundImage(document.querySelector('.bg-image'), release.image);
+
   const discogsSearchUrl = getSearchDiscogsReleaseUrl(
     release.artist,
     release.title
@@ -76,7 +52,7 @@ function addReleaseCardToElement(release, element) {
     )
     .join('<br>');
 
-  const releaseHeadline = createElementFromHTML(`
+  const template = `
 <div class="release-headline">
   <h1 id="release-artist" class="display-3">${release.artist}</h1>
   <h2 id="release-title" class="display-6">${release.title}</h2>
@@ -85,16 +61,21 @@ function addReleaseCardToElement(release, element) {
     <a href="${release.url}" title="Open Bandcamp release page\n${release.url}" target="_blank">Bandcamp</a>
     <a href="${discogsSearchUrl}" title="Search release on Discogs" target="_blank">Search on Discogs</a>
   </div>
-</div>`);
-
-  const releaseContent = createElementFromHTML(`
+</div>
 <div class="release-content small overflow-auto">
   <div id="release-tracks">${tracks}</div>
-</div>`);
+</div>`;
 
-  element.innerHTML = '';
-  element.appendChild(releaseHeadline);
-  element.appendChild(releaseContent);
+  element.innerHTML = template;
 
-  return element;
+  const releaseArtist = element.querySelector('#release-artist');
+  const releaseTitle = element.querySelector('#release-title');
+  const countArtistLines = countLinesInHtmlElement(releaseArtist);
+  const countTitleLines = countLinesInHtmlElement(releaseTitle);
+
+  releaseArtist.classList.toggle(
+    'display-6',
+    countArtistLines >= 3 && countArtistLines <= 5
+  );
+  element.classList = 'lines-a' + countArtistLines + '-t' + countTitleLines;
 }

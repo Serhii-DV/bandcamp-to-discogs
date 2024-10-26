0470535dfad837f3511b9578f91a216074b705ae
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2024-10-26 22:36:42 +0200

    Introduced global types

---
+++ src/types/global.d.ts
@@ -0,0 +1,27 @@
+export type Uuid = string;
+export interface UuidMap {
+  [key: Uuid]: any;
+}
+
+interface IArtistItem {
+  name: string;
+  url: string;
+  uuid: string;
+  image?: string;
+  visit?: Date;
+  id?: number;
+}
+
+interface IReleaseItem {
+  url: string;
+  artist: string;
+  title: string;
+  label?: string;
+  visit?: Date;
+  id?: number;
+}
+
+interface IMusic {
+  artist: IArtistItem;
+  albums: IReleaseItem[];
+}

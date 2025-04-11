387c2f59be44b4d57b5548b4207add5a5987985e
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2025-04-11 22:18:58 +0200

    Improve logic

--- src/bandcamp/modules/keywordMapping.ts
+++ src/bandcamp/modules/keywordMapping.ts
@@ -1,6 +1,6 @@
-import { hasOwnProperty, isArray, isEmptyObject } from '../../utils/utils';
-import keywordMapping from '../../data/keyword_mapping.json';
 import { Style } from '../../discogs/app/style';
+import keywordMapping from '../../data/keyword_mapping.json';
+import { hasOwnProperty, isArray, isEmptyObject } from '../../utils/utils';
 
 interface KeywordMapping {
   [key: string]: string | string[] | Style;
@@ -8,27 +8,29 @@ interface KeywordMapping {
 
 let mapping: KeywordMapping = {};
 
+/**
+ * Retrieves the keyword mapping, creating it if necessary.
+ */
 export function getKeywordMapping(): KeywordMapping {
   if (isEmptyObject(mapping)) {
     mapping = createMapping(keywordMapping);
   }
-
   return mapping;
 }
 
+/**
+ * Creates a keyword mapping based on the provided keyword mapping data.
+ */
 function createMapping(
   keywordMapping: Record<string, string | string[]>
 ): KeywordMapping {
-  const mapping: KeywordMapping = {};
+  const newMapping: KeywordMapping = {};
 
-  for (const key in keywordMapping) {
-    if (hasOwnProperty(keywordMapping, key)) {
-      const value = keywordMapping[key];
-      if (value !== '') {
-        mapping[key] = isArray(value) ? value : new Style(value as string);
-      }
+  Object.entries(keywordMapping).forEach(([key, value]) => {
+    if (hasOwnProperty(keywordMapping, key) && value !== '') {
+      newMapping[key] = isArray(value) ? value : new Style(value as string);
     }
-  }
+  });
 
-  return mapping;
+  return newMapping;
 }
